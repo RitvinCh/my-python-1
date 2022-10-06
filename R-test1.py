d9f0efc38763b346752ce85122958b0960cb9a8d
RitvinCh <115097930+RitvinCh@users.noreply.github.com>
2022-10-06 23:03:12 +0530

    Add files via upload

---
+++ R-test1.py
@@ -0,0 +1,9 @@
+number=int(input("Enter Your number :"))
+if((number%2)==0):
+ {
+    print("The given number is an Even Number!")
+ }
+else:
+ {
+    print("The given number is an Odd Number!")
+ }
